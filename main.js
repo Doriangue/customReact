280acfc24df29629a98e34d13e6f366e559d541d
Loan Besson <loan.besson@hotmail.fr>
2021-02-11 15:07:16 +0100

    On s'assure que la div root existe bien dans la page

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ import { route, router} from "./router.js"
 
 // on initialise la navbar en main router
 reactDOM.render(NavbarComponent, document.getElementById("root"), {router});
+
+//promise pour s'assurer que la div #root existe bien
+let promise = new Promise((resolve, reject) => {
+    let content = document.querySelector('#root');
+    if(content){
+        resolve('Ça fonctionne !');
+    } else{
+        reject('Ça fonctionne pas !');
+    }
+});
