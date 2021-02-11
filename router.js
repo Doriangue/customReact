9885691b2dcfc8e1ecda8883f1c6f62f9d2e311b
Loan Besson <loan.besson@hotmail.fr>
2021-02-11 14:58:11 +0100

    Exportation de la route demandée

--- router.js
+++ router.js
@@ -5,3 +5,9 @@ export var router = new mainRouter("mainRouter", [
   new Route("Accueil", "/"),
   new Route("Api", "/api")
 ])
+
+//On export la premiere occurence de notre tableau (parce que filter renvoie un array)
+//route pour get les deux Route class avec nos valeur
+export var route = router.routes.filter(function(route){
+  return route.getPath() === window.location.pathname; //on affecte la route de notre component a la path du navigateur
+})[0];
